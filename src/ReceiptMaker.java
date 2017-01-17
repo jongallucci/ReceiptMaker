2fc5114d364e8f361761a6e44637c25fbd8f96f6
Jonathon Gallucci <JonathonGallucci@gmail.com>
2017-01-17 00:03:35 -0500

    Added pricing, HST, Total price, and readouts

--- src/ReceiptMaker.java
+++ src/ReceiptMaker.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.math.*;
 
 public class ReceiptMaker{
 
 	public static void main(String[] args){
 		Scanner reader = new Scanner(System.in);	//setup to read user inputs
 		String itemBought = "";
-		List<String> itemList = new ArrayList<String>();	//list to hold all items bought by user
+		List<String> itemList = new ArrayList<String>();	//list of all items bought by user
 		
 		System.out.println("Enter items bought, write 'end' when done: ");
 		while(!itemBought.equals("end")){
@@ -15,7 +16,25 @@ public class ReceiptMaker{
 			itemList.add(itemBought);			//adds items to list
 		}
 		itemList.remove(itemList.size()-1);		//removes 'end' from list
-		System.out.println(itemList);
-		System.out.println("Done.");
+		
+		List<String> itemPrices = new ArrayList<String>();	//list of prices of items
+		for(int i =0; i < itemList.size(); i++){
+			String item = itemList.get(i);
+			double price = Math.round(Math.random()*item.length())+0.99;	//price determined by randomness and length of item name
+			itemPrices.add(Double.toString(price));		//adds price to price list
+		}
+		
+		double total = 0;						//total price of all items
+		System.out.println("\n\n\nHere is your receipt:\n");
+		
+		for(int i = 0; i < itemList.size(); i++){		//print out item with corresponding price, and add total up
+			System.out.println(itemList.get(i) + "\t\t$" + itemPrices.get(i));
+			total += Double.parseDouble(itemPrices.get(i));
+		}
+				//print receipt out nice and pretty
+		System.out.println("\nSubtotal:\t$" + Double.toString(total) + "\nHST: \t\t$" + Double.toString(((double)Math.round(total*13))/100));
+		System.out.println("Your total:\t$" + Double.toString(total+(double)Math.round(total*13)/100));
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("     Thank You!");
 	}
 }
