047cf584450ce1b677044c36351c015de6fae060
Jonathon Gallucci <JonathonGallucci@gmail.com>
2017-01-16 22:35:00 -0500

    Commented code

--- src/ReceiptMaker.java
+++ src/ReceiptMaker.java
@@ -5,18 +5,17 @@ import java.util.Scanner;
 public class ReceiptMaker{
 
 	public static void main(String[] args){
-		Scanner reader = new Scanner(System.in);
+		Scanner reader = new Scanner(System.in);	//setup to read user inputs
 		String itemBought = "";
-		List<String> itemList = new ArrayList<String>();
+		List<String> itemList = new ArrayList<String>();	//list to hold all items bought by user
 		
 		System.out.println("Enter items bought, write 'end' when done: ");
 		while(!itemBought.equals("end")){
-			itemBought = reader.nextLine();
-			itemList.add(itemBought);
+			itemBought = reader.nextLine();		//reads user inputs and stores them
+			itemList.add(itemBought);			//adds items to list
 		}
-		
+		itemList.remove(itemList.size()-1);		//removes 'end' from list
 		System.out.println(itemList);
 		System.out.println("Done.");
 	}
 }
-
