bf74421b774f5bac5d2ddfc75a066bc3f9afb734
Jonathon Gallucci <JonathonGallucci@gmail.com>
2017-01-18 17:35:36 -0500

    Fixed printing error / rounding error. Added comments

--- src/ReceiptMaker.java
+++ src/ReceiptMaker.java
@@ -20,21 +20,24 @@ public class ReceiptMaker{
 		List<String> itemPrices = new ArrayList<String>();	//list of prices of items
 		for(int i =0; i < itemList.size(); i++){
 			String item = itemList.get(i);
-			double price = Math.round(Math.random()*item.length())+0.99;	//price determined by randomness and length of item name
-			itemPrices.add(Double.toString(price));		//adds price to price list
+			float price = (float)(Math.round(Math.random()*item.length())+0.99);	//price determined by randomness and length of item name
+			itemPrices.add(Float.toString(price));		//adds price to price list
 		}
 		
-		double total = 0;						//total price of all items
+		float total = 0;								//total price of all items
 		System.out.println("\n\n\nHere is your receipt:\n");
 		
 		for(int i = 0; i < itemList.size(); i++){		//print out item with corresponding price, and add total up
 			System.out.println(itemList.get(i) + "\t\t$" + itemPrices.get(i));
-			total += Double.parseDouble(itemPrices.get(i));
+			total += Float.parseFloat(itemPrices.get(i));
 		}
+		
 				//print receipt out nice and pretty
-		System.out.println("\nSubtotal:\t$" + Double.toString(total) + "\nHST: \t\t$" + Double.toString(((double)Math.round(total*13))/100));
-		System.out.println("Your total:\t$" + Double.toString(total+(double)Math.round(total*13)/100));
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~");
-		System.out.println("     Thank You!");
+		double hst = total*0.13;						//calculates the taxes on purchases
+		double finalTotal = total+(total*0.13);			//calculates final total due
+		System.out.printf("\nSubtotal:\t$%.2f%n", total);
+		System.out.printf("HST: \t\t$%.2f%n", hst);
+		System.out.printf("Your total:\t$%.2f%n",finalTotal);
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~\n     Thank You!");
 	}
 }
